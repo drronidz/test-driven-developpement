0434d24da92fff4c830cde34119bd110ff26ff8e
drronidz <abdelhalim.diab@univ-constantine2.dz>
2021-01-18 23:05:49 +0100

    dollar side effects fixed. closes #4

--- src/test/java/drronidz/springframework/MoneyTest.java
+++ src/test/java/drronidz/springframework/MoneyTest.java
@@ -14,7 +14,9 @@ import static org.junit.jupiter.api.Assertions.*;
     @Test
     void multiplicationTest() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10,five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10,product.amount);
+        product = five.times(3);
+        assertEquals(15,product.amount);
     }
 }
